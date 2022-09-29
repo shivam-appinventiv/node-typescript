997a9ca74cacde933b3f108bb138409007e868e5
shivam-appinventiv <shivam.yadav1@appinventiv.com>
2022-09-29 16:57:05 +0530

    get and post routes created

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 
 // Import the express in typescript file
 import express from 'express';
+const bodyParser = require("body-parser");
  
 // Initialize the express engine
 const app: express.Application = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
  
 // Take a port 3000 for running server.
 const port: number = 3001;
@@ -12,6 +15,17 @@ const port: number = 3001;
 app.get('/', (_req, _res) => {
     _res.send("TypeScript With Expresss v1.8.9 from master branch");
 });
+
+app.get('/getRequest', (_req, _res) => {
+    _res.send("TypeScript With Expresss v1.8.9 from master branch");
+});
+
+app.post('/postRequest',(_req,_res)=>{
+    
+      let obj = JSON.stringify(_req.body)
+      console.log(`value of obj is ${obj}`);
+    _res.send(JSON.stringify(_req.body))
+})
  
 // Server setup
 app.listen(port, () => {
